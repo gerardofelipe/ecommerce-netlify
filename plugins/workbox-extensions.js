827750160be1be62a559448fb063069a04b0e14f
Gerardo <gerardo.felipe.conrado@experoinc.com>
2019-10-28 23:57:59 +0000

    Add Google fonts to the cache during the service worker installation.

--- plugins/workbox-extensions.js
+++ plugins/workbox-extensions.js
@@ -1,4 +1,18 @@
 // --------------------------------------------------
+// Manual cache on install event
+// --------------------------------------------------
+self.addEventListener('install', event => {
+  const googleFontsUrl = [
+    'https://fonts.googleapis.com/css?family=Montserrat:300,600|PT+Serif&display=swap',
+    'https://fonts.gstatic.com/s/montserrat/v14/JTURjIg1_i6t8kCHKm45_bZF3gnD_g.woff2',
+    'https://fonts.gstatic.com/s/montserrat/v14/JTURjIg1_i6t8kCHKm45_cJD3gnD_g.woff2',
+    'https://fonts.gstatic.com/s/ptserif/v11/EJRVQgYoZZY2vCFuvAFWzr8.woff2',
+  ]
+
+  event.waitUntil(caches.open('google-fonts').then(cache => cache.addAll(googleFontsUrl)))
+})
+
+// --------------------------------------------------
 // Messages
 // --------------------------------------------------
 self.addEventListener('message', event => {
