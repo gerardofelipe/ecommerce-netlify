fa07d3adc76fe39892bc3d88ee89fa28d75d7e3e
Gerardo <gerardo.felipe.conrado@experoinc.com>
2019-11-05 12:28:36 +0000

    feat(store): add OneSignal tags to the user profile to segment the notifications

--- store/index.js
+++ store/index.js
@@ -1,17 +1,18 @@
-import axios from "axios"
-import uuidv1 from "uuid/v1"
-import data from "~/static/storedata.json"
+import axios from 'axios'
+import uuidv1 from 'uuid/v1'
+import data from '~/static/storedata.json'
+import {addOneSignalCartTags, clearOneSignalCartTags} from "../utils/oneSignal";
 
 export const state = () => ({
-  cartUIStatus: "idle",
+  cartUIStatus: 'idle',
   storedata: data,
-  cart: []
+  cart: [],
 })
 
 export const getters = {
   featuredProducts: state => state.storedata.slice(0, 3),
-  women: state => state.storedata.filter(el => el.gender === "Female"),
-  men: state => state.storedata.filter(el => el.gender === "Male"),
+  women: state => state.storedata.filter(el => el.gender === 'Female'),
+  men: state => state.storedata.filter(el => el.gender === 'Male'),
   cartCount: state => {
     if (!state.cart.length) return 0
     return state.cart.reduce((ac, next) => ac + next.quantity, 0)
@@ -19,7 +20,7 @@ export const getters = {
   cartTotal: state => {
     if (!state.cart.length) return 0
     return state.cart.reduce((ac, next) => ac + next.quantity * next.price, 0)
-  }
+  },
 }
 
 export const mutations = {
@@ -27,52 +28,53 @@ export const mutations = {
     state.cartUIStatus = payload
   },
   clearCart: state => {
+    const ids = state.cart.map(({ id }) => id)
+    clearOneSignalCartTags(ids)
     //this clears the cart
-    ;(state.cart = []), (state.cartUIStatus = "idle")
+    ;(state.cart = []), (state.cartUIStatus = 'idle')
   },
   addToCart: (state, payload) => {
     let itemfound = state.cart.find(el => el.id === payload.id)
-    itemfound
-      ? (itemfound.quantity += payload.quantity)
-      : state.cart.push(payload)
-  }
+    itemfound ? (itemfound.quantity += payload.quantity) : state.cart.push(payload)
+    addOneSignalCartTags(itemfound || payload)
+  },
 }
 
 export const actions = {
   async postStripeFunction({ getters, commit }, payload) {
-    commit("updateCartUI", "loading")
+    commit('updateCartUI', 'loading')
 
     try {
       await axios
         .post(
-          "https://ecommerce-netlify.netlify.com/.netlify/functions/index",
+          'https://ecommerce-netlify.netlify.com/.netlify/functions/index',
           {
             stripeEmail: payload.stripeEmail,
             stripeAmt: Math.floor(getters.cartTotal * 100), //it expects the price in cents, as an integer
-            stripeToken: "tok_visa", //testing token, later we would use payload.data.token
-            stripeIdempotency: uuidv1() //we use this library to create a unique id
+            stripeToken: 'tok_visa', //testing token, later we would use payload.data.token
+            stripeIdempotency: uuidv1(), //we use this library to create a unique id
           },
           {
             headers: {
-              "Content-Type": "application/json"
-            }
-          }
+              'Content-Type': 'application/json',
+            },
+          },
         )
         .then(res => {
           if (res.status === 200) {
-            commit("updateCartUI", "success")
-            setTimeout(() => commit("clearCart"), 5000)
+            commit('updateCartUI', 'success')
+            setTimeout(() => commit('clearCart'), 5000)
           } else {
-            commit("updateCartUI", "failure")
+            commit('updateCartUI', 'failure')
             // allow them to try again
-            setTimeout(() => commit("updateCartUI", "idle"), 5000)
+            setTimeout(() => commit('updateCartUI', 'idle'), 5000)
           }
 
           console.log(JSON.stringify(res, null, 2))
         })
     } catch (err) {
       console.log(err)
-      commit("updateCartUI", "failure")
+      commit('updateCartUI', 'failure')
     }
-  }
+  },
 }
--- utils/oneSignal.js
+++ utils/oneSignal.js
@@ -8,3 +8,25 @@ export const oneSignalWorkerHack = () => {
     if (err) console.error(err)
   })
 }
+
+export const addOneSignalCartTags = (item = {}) => {
+  if (!item.id) return
+  OneSignal.push(() => {
+    OneSignal.sendTags({
+      [item.id]: item.quantity || '',
+      cart_update: Date.now(),
+    }).then(function(tagsSent) {
+      // Callback called when tags have finished sending
+      // console.log('tagsSent: ', tagsSent)
+    })
+  })
+}
+
+export const clearOneSignalCartTags = (idsToClean = []) => {
+  OneSignal.push(() => {
+    OneSignal.deleteTags([...idsToClean, cart_update]).then(function(tagsDeleted) {
+      // Callback called when tags have finished sending
+      // console.log('tagsDeleted: ', tagsDeleted)
+    })
+  })
+}
