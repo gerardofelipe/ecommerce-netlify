adc0291da9f29f96b47aa43d73786c706010b610
Gerardo <gerardo.felipe.conrado@experoinc.com>
2019-11-09 09:41:41 +0000

    fix(OneSignalWorker): fix the sw.js import in the first install

--- hooks/build.js
+++ hooks/build.js
@@ -1,7 +1,17 @@
-import { oneSignalWorkerHack } from '../utils/oneSignal'
+// import { oneSignalWorkerHack } from '../utils/oneSignal'
 import { createPrecacheManifest } from '../utils/precacheManifest'
 import { getTemplatedURLs } from '../utils/routes'
 import { getManifest } from 'workbox-build'
+import { copy } from 'fs-extra'
+import path from 'path'
+
+export const oneSignalWorkerHack = () => {
+  const src = path.join('static', 'OneSignalSDKWorker.js')
+  const dest = path.join('static', 'OneSignalSDKUpdaterWorker.js')
+  copy(src, dest, { overwrite: true }, err => {
+    if (err) console.error(err)
+  })
+}
 
 export default nuxtConfig => ({
   /**
--- utils/oneSignal.js
+++ utils/oneSignal.js
@@ -1,14 +1,3 @@
-import { copy } from 'fs-extra'
-import path from 'path'
-
-export const oneSignalWorkerHack = () => {
-  const src = path.join('static', 'OneSignalSDKWorker.js')
-  const dest = path.join('static', 'OneSignalSDKUpdaterWorker.js')
-  copy(src, dest, { overwrite: true }, err => {
-    if (err) console.error(err)
-  })
-}
-
 export const addOneSignalCartTags = (item = {}) => {
   if (!item.id) return
   OneSignal.push(() => {
